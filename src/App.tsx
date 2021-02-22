002d86a7178c5b38342f81c120134f3ad9dcadd3
Gevindu Mallikarachchi <32522094+gevinduM@users.noreply.github.com>
2021-02-21 20:55:02 -0500

    add custom radio field

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Field, Form, useField, FieldAttributes } from 'formik';
+import { TextField,Button, Checkbox, Radio, FormControlLabel } from '@material-ui/core';
+
+type myRadioProps = { label : string } & FieldAttributes<{}>;
+
+const MyRadio: React.FC<myRadioProps> = ({label, ...props})=>{
+  const [field] = useField<{}>(props);
+  return <FormControlLabel {...field} control={<Radio/>} label={label} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formik 
+        initialValues={{
+          firstName : "Gevindu Mallikarachchi",
+          isTall : false,
+          cookies :[],
+          yougurt:""
+        }
+        } 
+        onSubmit={(data, {setSubmitting})=>{
+          setSubmitting(true);
+          console.log(data);
+          setSubmitting(false)
+        }}
+      >
+        {
+          ({values,isSubmitting})=>(
+              <Form>
+                <Field name="firstName"  as={TextField}/>
+                <Field name="isTall" type="checkbox" as={Checkbox}/>
+                <div>Cookies: </div>
+                <Field name="cookies" type="checkbox"  as={Checkbox} value="Chock"/>
+                <Field name="cookies" type="checkbox" as={Checkbox} value="Vanila"/>
+                <Field name="cookies" type="checkbox" as={Checkbox} value="Banana"/>
+                <div>Yourgurt: </div>
+                <MyRadio name="yougurt" type="radio"  as={Radio} value="A" label="A"/>
+                <MyRadio name="yougurt" type="radio"  as={Radio} value="B" label="B"/>
+                <MyRadio name="yougurt" type="radio"  as={Radio} value="C" label="C"/>
+                <div>
+                  <Button disabled={isSubmitting}  type="submit">Submit</Button>
+                </div>
+              </Form>
+          )
+        }
+      </Formik>
     </div>
   );
 }
